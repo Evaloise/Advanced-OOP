37eae02134c471685b5b09cb74b4f4c32d68d7a6
Evaloise <125451274+Evaloise@users.noreply.github.com>
2024-08-03 17:01:46 +0300

    Update ArrayOperations.java

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -5,21 +5,18 @@ public class ArrayOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] array = new int[15];
-
-        // a) Get 15 integers from the user and store them in the array
+        
         System.out.println("Enter 15 integers:");
         for (int i = 0; i < array.length; i++) {
             array[i] = scanner.nextInt();
         }
 
-        // b) Print the array elements
         System.out.println("Array elements:");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
 
-        // c) Check if a number is present in the array
         System.out.print("Enter a number to search for: ");
         int searchNumber = scanner.nextInt();
         boolean found = false;
@@ -33,8 +30,6 @@ public class ArrayOperations {
         if (!found) {
             System.out.println("Number not found in this array.");
         }
-
-        // d) Sort the array in ascending order
         Arrays.sort(array);
         System.out.println("Sorted array:");
         for (int i = 0; i < array.length; i++) {
@@ -42,7 +37,6 @@ public class ArrayOperations {
         }
         System.out.println();
 
-        // e) Create a new array with elements in reverse order
         int[] reversedArray = new int[array.length];
         for (int i = 0; i < array.length; i++) {
             reversedArray[i] = array[array.length - 1 - i];
@@ -53,7 +47,6 @@ public class ArrayOperations {
         }
         System.out.println();
 
-        // f) Calculate the sum and product of array elements
         int sum = 0;
         int product = 1;
         for (int i = 0; i < array.length; i++) {
